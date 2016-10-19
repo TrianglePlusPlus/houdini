d58bbaf439e4d560337875e39732720dc013f462
Justice Suh <justice.suh@gmail.com>
2016-10-19 15:15:05 -0400

    Some models

--- houdini/core/models.py
+++ houdini/core/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth import hashers
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Permission(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	slug = models.SlugField()
+
+	@classmethod
+	def create(cls, name):
+		permission = cls(name=name)
+		return permission
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Permission, self).save(*args, **kwargs)
+
+
+class Role(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	slug = models.SlugField()
+	parents = models.ManyToManyField('self')
+
+	@classmethod
+	def create(cls, name):
+		role = cls(name=name)
+		return role
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Role, self).save(*args, **kwargs)
+
+class BaseUser(models.Model):
+	first_name = models.CharField(max_length=128)
+	last_name = models.CharField(max_length=128)
+	email = models.CharField(max_length=128)
+	password = models.CharField(max_length=128)
+
+	def set_password(self, password):
+		self.password = hashers.make_password(password)
+
+class Employee(BaseUser):
+	pass
--- houdini/core/views.py
+++ houdini/core/views.py
@@ -4,5 +4,17 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'core/index.html')
 
+def applications(request):
+	return render(request, 'core/base.html')
+
+def directories(request):
+	return render(request, 'core/directories.html')
+
 def users(request):
 	return render(request, 'core/users.html')
+
+def roles(request):
+	return render(request, 'core/base.html')
+
+def permissions(request):
+	return render(request, 'core/base.html')
