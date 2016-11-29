34a5dc7aea1b4c9bd4c85b34491945fa457f3f65
Justice Suh <justice.suh@gmail.com>
2016-11-28 21:13:25 -0500

    Role and Permission models

--- houdini/core/models.py
+++ houdini/core/models.py
@@ -1,59 +1,39 @@
-from django.contrib.auth import hashers
-from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.template.defaultfilters import slugify
-
-
-class UserType(models.Model):
-    name = models.CharField(max_length=32, unique=True)
-    fields = JSONField()
 
 
 class User(models.Model):
     pass
 
 
+class Employee(models.Model):
+    pass
+
+
+class Applicant(models.Model):
+    pass
+
+
+class Customer(models.Model):
+    pass
+
+
+class Alumnus(models.Model):
+    pass
+
+
 class Role(models.Model):
     name = models.CharField(max_length=64, unique=True)
     slug = models.SlugField()
-    permissions = JSONField()
-    parents = JSONField()
 
     @classmethod
     def create(cls, name):
-        role = cls(name=name, permissions=[], parents=[])
+        role = cls(name=name)
         return role
-
-    @classmethod
-    def get_json(cls):
-        data = {}
-        roles = Role.objects.all()
-        for role in roles:
-            data[role.slug] = {
-                "permissions": role.permissions,
-                "parents": role.parents
-            }
-        return data
-
-    def add_permission(self, slug):
-        if slug not in self.permissions:
-            self.permissions.append(slug)
-
-    def remove_permission(self, slug):
-        if slug in self.permissions:
-            self.permissions.remove(slug)
-
-    def add_parent(self, slug):
-        if slug not in self.parents:
-            self.parents.append(slug)
-
-    def remove_parent(self, slug):
-        if slug in self.parents:
-            self.parents.remove(slug)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Role, self).save(*args, **kwargs)
+        self.super(Role, self).save(*args, **kwargs)
 
 
 class Permission(models.Model):
@@ -67,4 +47,4 @@ class Permission(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Permission, self).save(*args, **kwargs)
+        self.super(Permission, self).save(*args, **kwargs)
