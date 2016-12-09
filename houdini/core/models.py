a69a6a8b22e663c42900a7371eb58fe74bd186f6
Justice Suh <justice.suh@gmail.com>
2016-12-08 20:23:16 -0500

    Add User profiles

--- houdini/core/models.py
+++ houdini/core/models.py
@@ -1,13 +1,48 @@
+import json
+import uuid
+
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.utils import timezone
 
 
-class User(AbstractBaseUser):
-    class Meta:
-        abstract = True
+class Application(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    app_key = models.CharField(max_length=32)
+    app_secret = models.CharField(max_length=32)
 
+    # JSON serialized string of profile names
+    profiles = models.TextField()
+
+    def generate_app_key(self):
+        self.app_key = uuid.uuid4().hex
+
+    def generate_app_secret(self):
+        self.app_secret = uuid.uuid4().hex
+
+    def add_profile(self, profile_name):
+        """
+        add profile to application
+        """
+        profile_names = json.loads(self.profiles)
+        if profile_name not in profile_names:
+            profile_names.append(profile_name)
+            profile_names.sort()
+            self.profiles = json.dumps(profile_names)
+
+    def remove_profile(self, profile_name):
+        """
+        remove profile from application
+        """
+        profile_names = json.loads(self.profiles)
+        if profile_name in profile_names:
+            profile_names.remove(profile_name)
+            profile_names.sort()
+            self.profiles = json.dumps(profile_names)
+
+
+class User(AbstractBaseUser):
     first_name = models.CharField(max_length=32)
     middle_name = models.CharField(max_length=32, null=True)
     last_name = models.CharField(max_length=32)
@@ -24,7 +59,20 @@ class User(AbstractBaseUser):
     zip = models.CharField(max_length=11, null=True)
 
 
-class Employee(User):
+class Profile(models.Model):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_all_profiles(cls):
+        """
+        get all available profile subclasses
+        """
+        return [profile.__name__ for profile in cls.__subclasses__]
+
+
+class EmployeeProfile(Profile):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     phone = models.CharField(max_length=24)
     dob = models.CharField(max_length=24)
     classyear = models.IntegerField()
@@ -36,7 +84,8 @@ class Employee(User):
     home_service = models.IntegerField()
 
 
-class Applicant(User):
+class ApplicantProfile(Profile):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     classyear = models.IntegerField()
     school = models.CharField(max_length=3)
     major = models.CharField(max_length=128)
@@ -46,11 +95,12 @@ class Applicant(User):
     # add more applicant specific fields
 
 
-class Customer(User):
-    pass
+class CustomerProfile(Profile):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
-class Alumnus(User):
+class AlumnusProfile(Profile):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     phone = models.CharField(max_length=24)
     dob = models.CharField(max_length=24)
     classyear = models.IntegerField()
@@ -60,14 +110,57 @@ class Alumnus(User):
     sex = models.CharField(max_length=32)
 
 
+# TODO: figure out how to sync updates between
+# role and permission tables and graph database
 class Role(models.Model):
     name = models.CharField(max_length=64, unique=True)
     slug = models.SlugField()
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
+    permissions = models.ManyToManyField(Permission)
 
     @classmethod
     def create(cls, name):
         role = cls(name=name)
         return role
+
+    @classmethod
+    def get_json(cls):
+        """
+        get json for all role objects in database
+        """
+        roles = Role.objects.all()
+        json = {}
+        for role in roles:
+            json[role.slug] = role.get_json()
+        return json
+
+    def get_json(self):
+        """
+        get json representation of this object for graph
+        """
+        return {
+            'parents': self.get_parents(),
+            'permissions': self.get_permissions()
+        }
+
+    # this method is super inefficient
+    def get_parents(self):
+        """
+        get a list of slugs of this roles parents
+        (traverse up the graph)
+        """
+        parents = []
+        node = self.parent
+        while node:
+            parents.append(node.slug)
+            node = node.parent
+        return parents
+
+    def get_permissions(self):
+        """
+        get a list of slugs of this roles permissions
+        """
+        return [permission.slug for permission in permissions]
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
