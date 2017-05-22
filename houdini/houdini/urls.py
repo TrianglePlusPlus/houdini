9b0a6e4315e65b785bca8a38f6fa98fa999b31d3
peterthejohnston <peter.johnston.xc@gmail.com>
2017-05-22 02:10:52 -0600

    restructure apps: admin -> houdini_admin, core -> houdini_server, client -> houdini_client

--- houdini/houdini/urls.py
+++ houdini/houdini/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('core.urls')),
-    url(r'^', include('client.urls')),
+    url(r'^', include('houdini_server.urls')),
+    url(r'^', include('houdini_client.urls')),
 ]
---
+++ houdini/houdini_client/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HoudiniClientConfig(AppConfig):
+    name = 'houdini_client'
